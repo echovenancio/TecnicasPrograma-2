af5268046610394ef88e49da277186b83e8a6240
laptopfatec <fatec@teste.com>
2024-10-16 14:32:33 -0300

    filter product title

--- Pratica7/src/main/java/App.java
+++ Pratica7/src/main/java/App.java
@@ -10,13 +10,15 @@ import java.util.logging.Filter;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("****SISTEMA DE BUSCA DE PRODUTOS EM PROMOÇÂO");
+        System.out.println("****SISTEMA DE BUSCA DE PRODUTOS EM PROMOÇÂO****");
+        System.out.println("digite 'sair' para sair.");
         var sc = new Scanner(System.in);
         while (true) {
             System.out.print("Digite o preço máximo do produto ->> ");
             var input = sc.nextLine().split(" ");
             if (Objects.equals(input[0], "")) { continue; }
             else if (input.length > 1) { System.out.println("Não esperava mais de um argumento"); continue; }
+            if (Objects.equals(input[0], "sair")) { System.exit(0); }
             try {
                 int price = Integer.parseInt(input[0]);
                 App.printProducts(price);
@@ -30,15 +32,12 @@ public class App {
         var reqService = new RequestService();
         var productsInSale = FilterService.getProductsInSale(reqService.getProducts(), price);
         if (productsInSale.isEmpty()) {
-            System.out.println("Nenhum produto encontrado nessa faixa de preço");
+            System.out.println("Nenhum produto encontrado nessa faixa de preço.");
             return;
         }
         System.out.println("***********PRODUTOS IMPERDÍVEIS E EM PROMOÇÃO!***********");
-        for (Product product : productsInSale) {
-            System.out.println(String.format("%s EM *PROMOÇÂO*", product.title()).toUpperCase());
-            System.out.println(product.description());
-            System.out.println(String.format("POR APENAS R$%s!", product.price()));
-            System.out.println("--------------------------------------------------");
+        for (String productTitle : productsInSale) {
+            System.out.println(String.format("+ %s EM *PROMOÇÂO*", productTitle).toUpperCase());
         }
     }
 }
--- Pratica7/src/main/java/service/FilterService.java
+++ Pratica7/src/main/java/service/FilterService.java
@@ -3,9 +3,12 @@ package service;
 import model.Product;
 
 import java.util.List;
+import java.util.Stack;
 
 public class FilterService {
-    public static List<Product> getProductsInSale(List<Product> products, int price) {
-        return products.stream().filter(p -> p.price() < price).toList();
+    public static List<String> getProductsInSale(List<Product> products, int price) {
+        return products.stream()
+                .filter(p -> p.price() < price)
+                .map(Product::title).toList();
     }
 }
