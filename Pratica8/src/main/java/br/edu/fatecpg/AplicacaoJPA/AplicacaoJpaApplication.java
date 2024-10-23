d2079f7da6786eb3c2962a5a934a2fa260958469
laptopfatec <fatec@teste.com>
2024-10-23 16:13:05 -0300

    Finish Pratica8

---
+++ Pratica8/src/main/java/br/edu/fatecpg/AplicacaoJPA/AplicacaoJpaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.AplicacaoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoJpaApplication.class, args);
+	}
+
+}
---
+++ Pratica8/src/test/java/br/edu/fatecpg/AplicacaoJPA/AplicacaoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.AplicacaoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
